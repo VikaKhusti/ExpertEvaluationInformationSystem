780f4852a1fa512f921e31490780aa09a7a2a37f
Victoria Khusti <viktorijahusti@gmail.com>
2021-04-15 16:01:43 +0300

    initial commit

---
+++ ExpertEvaluationIS/Start Page.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExpertEvaluationIS
+{
+    public partial class Start_Page : Form
+    {
+        public Start_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
